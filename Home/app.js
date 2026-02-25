910643f40bc2efc54fc0b25630b9c45c5c3c503f
KemuelRG <kemuelkemuel3@gmail.com>
2026-02-25 19:32:13 +0000

    Home index created

--- Home/app.js
+++ Home/app.js
@@ -1,18 +1,18 @@
 // Función para cargar componentes HTML
 function cargarComponente(idContenedor, archivoHTML) {
-  fetch(archivoHTML)
-    .then((response) => {
-      if (!response.ok) throw new Error('Error al cargar ' + archivoHTML);
-      return response.text();
-    })
-    .then((html) => {
-      document.getElementById(idContenedor).innerHTML = html;
-    })
-    .catch((error) => console.error(error));
+    fetch(archivoHTML)
+        .then((response) => {
+            if (!response.ok) throw new Error('Error al cargar ' + archivoHTML);
+            return response.text();
+        })
+        .then((html) => {
+            document.getElementById(idContenedor).innerHTML = html;
+        })
+        .catch((error) => console.error(error));
 }
 
 // Ejecutamos la función cuando el DOM cargue
 document.addEventListener('DOMContentLoaded', () => {
-  cargarComponente('header-placeholder', 'header.html');
-  cargarComponente('footer-container', 'footer.html');
+    cargarComponente('header-container', 'header.html');
+    cargarComponente('footer-container', 'footer.html'); // Cambiado a -placeholder
 });
