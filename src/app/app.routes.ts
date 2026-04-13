c09132b7c4f63de6cf7f45183f0c68cf76feb795
OscarYavar <oscaryavarxd@gmail.com>
2026-04-13 17:49:00 +0100

    Added bio and explorer files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Bio } from './pages/biodata/bio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'bio', component: Bio, title: 'SPORTS&DIET · Bio Data' },
+];
