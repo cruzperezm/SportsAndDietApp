48a061a54bd49839703cff30923f8174cb54ead5
OscarYavar <oscaryavarxd@gmail.com>
2026-04-24 17:27:33 +0100

    Added connection to firebase

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,24 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule, Router } from '@angular/router'; // Importante para que funcionen tus botones
-import { AuthService } from '../../../services/auth.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [RouterModule, CommonModule],
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  templateUrl: './header.html',
+  styleUrls: ['./header.css'],
 })
 export class HeaderComponent {
-  private authService = inject(AuthService);
 
   private router = inject(Router);
 
-  isLoggedIn$ = this.authService.isLoggedIn$;
-
-  onLogout() {
-    this.authService.logout();
-    this.router.navigate(['/login']); // Redirect to login
-  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyCNzwzCKAZoEywB5T6XMT94hidbuxi7BAk',
+    authDomain: 'sportsanddietapp.firebaseapp.com',
+    databaseURL: 'https://sportsanddietapp-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'sportsanddietapp',
+    storageBucket: 'sportsanddietapp.firebasestorage.app',
+    messagingSenderId: '446766581454',
+    appId: '1:446766581454:web:4a7ae65fbc8dd9aeeb729f',
+    measurementId: 'G-5DWM0RNT5F',
+  },
+};
