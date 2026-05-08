6c360706731a56ef3eef487fb5df1e4c887b38d1
OscarYavar <oscaryavarxd@gmail.com>
2026-05-08 15:56:15 +0100

    Practicing Ionic

--- Ionic/src/app/home/home.page.ts
+++ Ionic/src/app/home/home.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent,
+  IonAccordion, IonAccordionGroup, IonItem, IonLabel, IonBadge,
+  IonRange, IonDatetime, IonRefresherContent, IonRefresher,
+  RefresherCustomEvent} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,
+    IonAccordion, IonAccordionGroup, IonItem, IonLabel, IonBadge,
+    IonRange, IonDatetime, IonRefresher, IonRefresherContent],
 })
 export class HomePage {
   constructor() {}
+   handleRefresh(event: RefresherCustomEvent) {
+      setTimeout(() => {
+        // Any calls to load data go here
+        event.target.complete();
+      }, 2000);
+    }
 }
